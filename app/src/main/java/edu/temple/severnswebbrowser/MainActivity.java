be404a93ca7ce032b290d783bf8b03cedb67ab03
daveseverns <david.w.severns@gmail.com>
2017-10-29 23:51:48 -0400

    hoping this works

--- app/src/main/java/edu/temple/severnswebbrowser/MainActivity.java
+++ app/src/main/java/edu/temple/severnswebbrowser/MainActivity.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    //member vars
     ViewPager viewPager;
     int currentIndex, lastIndex;
     EditText urlSearchBar;
